0890103b00f961e72522c6d4514afe6bc397858a
donghyun_daniel <donghyun.main@gmail.com>
2020-04-08 12:11:47 +0900

    Tower_defense update Tower build fin

--- Tower_Defense/Tower.py
+++ Tower_Defense/Tower.py
@@ -63,7 +63,6 @@ class Tower():  #range, dmg, (atk_speed is lower is better)
         self.pos_y = pos_y
         self.button = pygame.Rect(0, 0, 50, 50)
         self.button.center = (self.pos_x, self.pos_y)
-        self.price=0
 
     def upgrade(self, money, type, level):
         self.type=type
@@ -140,6 +139,10 @@ class Tower():  #range, dmg, (atk_speed is lower is better)
     def menu(self, mouse, event, screen, money):  # pos is the value in the map_pos
         build = []
         txt_font = pygame.font.SysFont('georgia', 20)
+        pygame.draw.rect(screen, (70, 70, 70), [self.button.centerx-75, self.button.centery-75, 150, 150])
+        if self.type>0:
+            screen.blit(self.image, self.image.get_rect(centerx=self.button.centerx, centery=self.button.centery))
+
         if self.type==0:
             for i in range(4):
                 build.append(Button(0, 0, 50, 50))
@@ -147,7 +150,7 @@ class Tower():  #range, dmg, (atk_speed is lower is better)
             build[1].button.center = (self.button.centerx - 50, self.button.centery - 50)
             build[2].button.center = (self.button.centerx + 50, self.button.centery - 50)
             build[3].button.center = (self.button.centerx - 50, self.button.centery + 50)
-            Button.add_image(build[0], "images/cancel.png", False)
+            Button.add_image(build[0], "images/cancel.png", "cancel")
             Button.add_image(build[1], "images/tower1_1.png", (1,1))
             Button.add_image(build[2], "images/tower2_1.png", (2,1))
             Button.add_image(build[3], "images/tower3_1.png", (3,1))
@@ -162,77 +165,121 @@ class Tower():  #range, dmg, (atk_speed is lower is better)
             screen.blit(t3, t3.get_rect(centerx=build[3].button.centerx, centery=build[3].button.centery + 40))
 
         else: #tower is exists
-            for i in range(2):
-                build.append(Button(0, 0, 50, 50))
-            build[0].button.center = (self.button.centerx + 50, self.button.centery)
-            build[1].button.center = (self.button.centerx - 50, self.button.centery)
+            if self.level!=3:
+                for i in range(3):
+                    build.append(Button(0, 0, 50, 50))
+                build[0].button.center = (self.button.centerx, self.button.centery+50)
+                build[1].button.center = (self.button.centerx-50, self.button.centery-50)
+                build[2].button.center = (self.button.centerx+50, self.button.centery-50)
+            else:
+                for i in range(2):
+                    build.append(Button(0, 0, 50, 50))
+                build[0].button.center = (self.button.centerx-50, self.button.centery)
+                build[1].button.center = (self.button.centerx+50, self.button.centery)
+
             if self.type==1:
                 if self.level==1:
                     Button.add_image(build[0], "images/tower1_2.png", (1,2))
-                    Button.add_image(build[1], "images/cancel.png", False)
+                    Button.add_image(build[1], "images/cancel.png", "cancel")
+                    Button.add_image(build[2], "images/sell.png", "sell")
                     c = txt_font.render('CANCEL', False, RED)
                     t1 = txt_font.render('${}'.format(tower_price[self.type-1][self.level]), True, YELLOW)
+                    t2 = txt_font.render('${}'.format(int(tower_price[self.type-1][self.level-1]*0.8)), True, YELLOW)
+                    screen.blit(t2, t2.get_rect(centerx=build[2].button.centerx, centery=build[2].button.centery + 40))
                     screen.blit(c, c.get_rect(centerx=build[1].button.centerx, centery=build[1].button.centery + 40))
                     screen.blit(t1, t1.get_rect(centerx=build[0].button.centerx, centery=build[0].button.centery + 40))
                 elif self.level==2:
                     Button.add_image(build[0], "images/tower1_3.png", (1,3))
-                    Button.add_image(build[1], "images/cancel.png", False)
+                    Button.add_image(build[1], "images/cancel.png", "cancel")
+                    Button.add_image(build[2], "images/sell.png", "sell")
                     c = txt_font.render('CANCEL', False, RED)
                     t1 = txt_font.render('${}'.format(tower_price[self.type - 1][self.level]), True, YELLOW)
+                    t2 = txt_font.render('${}'.format(int(tower_price[self.type-1][self.level-1]*0.8)), True, YELLOW)
+                    screen.blit(t2, t2.get_rect(centerx=build[2].button.centerx, centery=build[2].button.centery + 40))
                     screen.blit(c, c.get_rect(centerx=build[1].button.centerx, centery=build[1].button.centery + 40))
                     screen.blit(t1, t1.get_rect(centerx=build[0].button.centerx, centery=build[0].button.centery + 40))
+                elif self.level==3:
+                    Button.add_image(build[0], "images/cancel.png", "cancel")
+                    Button.add_image(build[1], "images/sell.png", "sell")
+                    c = txt_font.render('CANCEL', False, RED)
+                    t2 = txt_font.render('${}'.format(int(tower_price[self.type-1][self.level-1]*0.8)), True, YELLOW)
+                    screen.blit(c, c.get_rect(centerx=build[0].button.centerx, centery=build[0].button.centery + 40))
+                    screen.blit(t2, t2.get_rect(centerx=build[1].button.centerx, centery=build[1].button.centery + 40))
             elif self.type==2:
                 if self.level==1:
                     Button.add_image(build[0], "images/tower2_2.png", (2,2))
-                    Button.add_image(build[1], "images/cancel.png", False)
+                    Button.add_image(build[1], "images/cancel.png", "cancel")
+                    Button.add_image(build[2], "images/sell.png", "sell")
                     c = txt_font.render('CANCEL', False, RED)
                     t1 = txt_font.render('${}'.format(tower_price[self.type - 1][self.level]), True, YELLOW)
+                    t2 = txt_font.render('${}'.format(int(tower_price[self.type-1][self.level-1]*0.8)), True, YELLOW)
+                    screen.blit(t2, t2.get_rect(centerx=build[2].button.centerx, centery=build[2].button.centery + 40))
                     screen.blit(c, c.get_rect(centerx=build[1].button.centerx, centery=build[1].button.centery + 40))
                     screen.blit(t1, t1.get_rect(centerx=build[0].button.centerx, centery=build[0].button.centery + 40))
                 elif self.level==2:
                     Button.add_image(build[0], "images/tower2_3.png", (2,3))
-                    Button.add_image(build[1], "images/cancel.png", False)
+                    Button.add_image(build[1], "images/cancel.png", "cancel")
+                    Button.add_image(build[2], "images/sell.png", "sell")
                     c = txt_font.render('CANCEL', False, RED)
                     t1 = txt_font.render('${}'.format(tower_price[self.type - 1][self.level]), True, YELLOW)
+                    t2 = txt_font.render('${}'.format(int(tower_price[self.type-1][self.level-1]*0.8)), True, YELLOW)
+                    screen.blit(t2, t2.get_rect(centerx=build[2].button.centerx, centery=build[2].button.centery + 40))
                     screen.blit(c, c.get_rect(centerx=build[1].button.centerx, centery=build[1].button.centery + 40))
                     screen.blit(t1, t1.get_rect(centerx=build[0].button.centerx, centery=build[0].button.centery + 40))
+                elif self.level==3:
+                    Button.add_image(build[0], "images/cancel.png", "cancel")
+                    Button.add_image(build[1], "images/sell.png", "sell")
+                    c = txt_font.render('CANCEL', False, RED)
+                    t2 = txt_font.render('${}'.format(int(tower_price[self.type-1][self.level-1]*0.8)), True, YELLOW)
+                    screen.blit(c, c.get_rect(centerx=build[0].button.centerx, centery=build[0].button.centery + 40))
+                    screen.blit(t2, t2.get_rect(centerx=build[1].button.centerx, centery=build[1].button.centery + 40))
             elif self.type==3:
                 if self.level==1:
                     Button.add_image(build[0], "images/tower3_2.png", (3,2))
-                    Button.add_image(build[1], "images/cancel.png", False)
+                    Button.add_image(build[1], "images/cancel.png", "cancel")
+                    Button.add_image(build[2], "images/sell.png", "sell")
                     c = txt_font.render('CANCEL', False, RED)
                     t1 = txt_font.render('${}'.format(tower_price[self.type - 1][self.level]), True, YELLOW)
+                    t2 = txt_font.render('${}'.format(int(tower_price[self.type-1][self.level-1]*0.8)), True, YELLOW)
+                    screen.blit(t2, t2.get_rect(centerx=build[2].button.centerx, centery=build[2].button.centery + 40))
                     screen.blit(c, c.get_rect(centerx=build[1].button.centerx, centery=build[1].button.centery + 40))
                     screen.blit(t1, t1.get_rect(centerx=build[0].button.centerx, centery=build[0].button.centery + 40))
                 elif self.level==2:
                     Button.add_image(build[0], "images/tower3_3.png", (3,3))
-                    Button.add_image(build[1], "images/cancel.png", False)
+                    Button.add_image(build[1], "images/cancel.png", "cancel")
+                    Button.add_image(build[2], "images/sell.png", "sell")
                     c = txt_font.render('CANCEL', False, RED)
                     t1 = txt_font.render('${}'.format(tower_price[self.type - 1][self.level]), True, YELLOW)
+                    t2 = txt_font.render('${}'.format(int(tower_price[self.type-1][self.level-1]*0.8)), True, YELLOW)
+                    screen.blit(t2, t2.get_rect(centerx=build[2].button.centerx, centery=build[2].button.centery + 40))
                     screen.blit(c, c.get_rect(centerx=build[1].button.centerx, centery=build[1].button.centery + 40))
                     screen.blit(t1, t1.get_rect(centerx=build[0].button.centerx, centery=build[0].button.centery + 40))
+                elif self.level==3:
+                    Button.add_image(build[0], "images/cancel.png", "cancel")
+                    Button.add_image(build[1], "images/sell.png", "sell")
+                    c = txt_font.render('CANCEL', False, RED)
+                    t2 = txt_font.render('${}'.format(int(tower_price[self.type-1][self.level-1]*0.8)), True, YELLOW)
+                    screen.blit(c, c.get_rect(centerx=build[0].button.centerx, centery=build[0].button.centery + 40))
+                    screen.blit(t2, t2.get_rect(centerx=build[1].button.centerx, centery=build[1].button.centery + 40))
 
         for b in build:
             Button.draw(b,screen)
             clicked_button = Button.check_click(b,mouse,event,screen)
+            Tower.check_mouse_on(b,mouse,screen)
             if clicked_button == b:
-                if clicked_button.name:#some tower has been built
+                if clicked_button.name=="sell":
+                    money+=int(tower_price[self.type-1][self.level-1]*0.8)
+                    self.type=0
+                    return money
+                elif clicked_button.name=="cancel":
+                    return money
+                else:#some tower has been built
                     if money>=tower_price[clicked_button.name[0]-1][clicked_button.name[1]-1]:
                         Tower.upgrade(self, money, clicked_button.name[0], clicked_button.name[1])
                         return money-tower_price[clicked_button.name[0]-1][clicked_button.name[1]-1]
-                else:
-                    return money
         return False
 
 
-
-
-    def sell(self, money):
-        money+=(int(self.price*0.8))
-        self.level = 0
-        self.type = 0
-        return money
-
     def check_mouse_on(self, mouse, screen):
         if self.button.collidepoint(mouse):
-            pygame.draw.rect(screen, (255, 255, 0), self.button)
+            pygame.draw.rect(screen, (255, 255, 0), self.button, 4)
